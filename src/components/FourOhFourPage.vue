c32bae27f7caf68d63ae0726f302a3bde40a3b16
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-05-12 13:47:36 +0700

    chore(repo): clear repo for migration

--- src/components/FourOhFourPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="four-of-four-page text-lg">
-    <div class="max-w-md mx-auto px-5">
-    <h1 class="text-5xl text-indigo-dark mt-16 mb-10">Uh Oh, You are lost!</h1>
-      <p class="text-xl mb-12 text-grey-darkest">Seems like you are searching for something non existed.</p>
-      <p class="text-grey-darkest">Go back to <a href="/">homepage</a></p>
-    </div>
-  </div>
-</template>
