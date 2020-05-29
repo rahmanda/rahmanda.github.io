3b2c1b0029760dc1526e3ae98f7eeb8b973dd62b
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-05-29 18:36:07 +0700

    fix: setup lang attribute on blog hero

--- src/components/BlogHero.vue
+++ src/components/BlogHero.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="max-w-2xl mx-auto px-5 py-6">
     <h1 class="hidden" aria-hidden="true">{{ h1 }}</h1>
-    <p class="text-indigo-700 text-6xl font-sans-title font-bold tracking-tighter leading-tight">Just a Blog</p>
-    <p class="text-gray-600 text-2xl mb-5">by Rahmanda Wibowo</p>
+    <p lang="en" class="text-indigo-700 text-6xl font-sans-title font-bold tracking-tighter leading-tight">Just a Blog</p>
+    <p lang="en" class="text-gray-600 text-2xl mb-5">by Rahmanda Wibowo</p>
     <p class="text-gray-800 text-xl leading-tight">
       <slot>
         Collection of my writings in web development and other technology
